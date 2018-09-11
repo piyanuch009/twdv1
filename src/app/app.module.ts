062cdb9cdf82def566f32e1317ea7912eba15456
piyanuch009 <tuamaichana@gmail.com>
2018-09-11 21:09:22 +0700

    angular login and dashborad

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppComponent } from './app.component';
+import { AppfooterComponent } from './components/appfooter/appfooter.component';
+import { AppheaderComponent } from './components/appheader/appheader.component';
+import { AppsettingComponent } from './components/appsetting/appsetting.component';
+import { AppmenuComponent } from './components/appmenu/appmenu.component';
+import { LoginComponent } from './components/login/login.component';
+
+import { Routes,RouterModule } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import {FormsModule} from '@angular/forms'
+
+const routes: Routes = [
+	{path: '',redirectTo:'/login',pathMatch: 'full'},
+  {path: 'login',component:LoginComponent},
+  {path: 'dashboard',component:DashboardComponent},
+	{path:'**',redirectTo:'/login'}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppfooterComponent,
+    AppheaderComponent,
+    AppsettingComponent,
+    AppmenuComponent,
+    LoginComponent,
+    DashboardComponent
+   
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import {Router}from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  mUsername:String = "";
+  mPassword:String = "";
+ 
+  constructor(private router:Router) { }
+
+  ngOnInit() {
+  }
+
+  onClickSubmit(){
+    if (this.mUsername == "piyanut@support.com" && this.mPassword == "1234"){
+      this.router.navigate(["/dashboard"])
+    }else{
+      window.alert("Login Failed");
+    }
+  }
+
+}
